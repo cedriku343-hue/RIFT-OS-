54f9899a1b604e763fbb8abe3f1df76cc38311be
MacOS Distro <cedriku343@gmail.com>
2026-07-18 18:44:26 +0100

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,1 +1,11 @@
+void main_kernel_function(void) {
+    volatile char* VGA_MEM = (volatile char*)0xB8000;
+    const char* message = "Welcome to RIFT OS";
+    
+    for (int i = 0; message[i] != '\0'; i++) {
+        VGA_MEM[i * 2] = message[i];
+        VGA_MEM[i * 2 + 1] = 0x07;
+    }
+}
+
 
